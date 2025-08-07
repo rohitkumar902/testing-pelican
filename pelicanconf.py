3dc6a1def32d8a35c7a32a5b841d4cc7008cd51e
rohitkumar902 <anjaiswal9@gmail.com>
2025-08-07 11:14:45 +0530

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,1 +1,11 @@
+AUTHOR = 'Rohit'
+SITENAME = 'Testing Pelican'
+SITEURL = 'https://rohitkumar902.github.io/testing-pelican/'
+
+PATH = 'content'
+TIMEZONE = 'Asia/Kolkata'
+DEFAULT_LANG = 'en'
+
+THEME = 'notmyidea'  # Default theme
+DEFAULT_PAGINATION = 10
 
